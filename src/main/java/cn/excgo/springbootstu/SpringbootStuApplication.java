4c9a28e19c3ef8a361f7e749dd345e76e3beb08c
moci <firemumu@qq.com>
2018-09-18 17:47:51 +0800

    新建项目，初始化提交

---
+++ src/main/java/cn/excgo/springbootstu/SpringbootStuApplication.java
@@ -0,0 +1,12 @@
+package cn.excgo.springbootstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStuApplication.class, args);
+    }
+}
